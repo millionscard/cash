68b2439695960f14967553cfe8dc32e444d9f6cc
André Ruffert <github@andreruffert.com>
2021-09-23 13:07:51 +0200

    chore(storybook): setup aliases
    This is necessary to run react-native in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,19 @@
 module.exports = {
-  "stories": [
-    "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ['../storybook/stories/**/*.stories.mdx', '../storybook/stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-native$': 'react-native-web',
+      '@storybook/react-native': '@storybook/react',
+      'styled-components/native': 'styled-components',
+    };
+
+    // Return the altered config
+    return config;
+  },
+};
