019571128cf19845a075475b648bbc404e79dd6b
André Ruffert <github@andreruffert.com>
2021-09-23 13:14:46 +0200

    chore: update prettier config
    https://prettier.io/playground/#N4Igxg9gdgLgprEAuEAzArlMMCW0AEAEnADYkQDqEATiQCYAUwA5tXHLlM-gLz4A6IABalyggDT5W7eHV74A5IKq06ghZIDOOEghjzUAQxKa4k6AFkI6UwHkAbnGriAvgEp8wflG-58OVAYAQmkOHC43YDYYdGoofCh0MhcAbm9fPz8Aeiz8ABVbABFbJHxC6AV9Gzh8akMoOggAW394tganDN19RJa+C0MYIQA6VHIafAYBoeG6huaGDwAqfABGAFEAagB2N2GYCABlGGpw5kXZuAAHEkMwOAYs-n5h57pNrJxmSUFBN3T4rUOLF4gAeOg4ez4MC3TSaAByhiacB4CmIZEoNHoCnwuBguh4wAABgBNaz4QxsfD2HDaA7UBLoJoAIyc+AAJMBGS0XESXPhLNY7I5qITBdUHE4XAA+AGZfCgzQnaDMaVc0KcZjDTQkHD3Bj4AAMklW+D2BwAqlcrk4AMKGUyLfCbKRsMJcbW6-Wrc0QAAyEAA7naHQ8PC5QVkldQVbLAX4WG7NcMEHRNBQcEMGBI-vgAPwCECF-ClRVXer4JUATwJwGAkHI1FKSn40irChcMokhcjmnLUGl+BcGT8oLgTTj-Bg6qTcDkw58MEj47j8q5DG0ulg-3jmQLgleIBHmVKgiCggXx8jEPs0rSPigLhA4hAECuuGgmmQoEpMcDAAVKQQL8UEMewIBwOhnxAZk6jAABrDhDiROA-XCOBkCMEwzBguDEJgQ5yzAM5kBOdAcPHVk6DoOc-XqZh0EMZg4AAMRoJpBk1ZAQEMdADmgoQYCaEgKCETM4D7O44EOYDM0hTMq24sA4Wg8JTGoGB-zqZgOMw4xTBfAArTQAA9DjOXQAEV0AgeA9Owl9y2odTuOZQxWRIaCrlOWAMzoIZkFWQ1jRAbyIFMCg6iubjvIkpxHGggBHGz4C0t8QJ4zQAFooHYGioJfNhkpwNgtKY3SkCwgyQFMJocFI6hyJfbQuCslKMKQMicJgdy-ICpAACYXxOQwdDOW1mgqkAJIAVmg6o8nckCqpw+xyIASQ6WBDjAU53wAQQaY4aw6lbOyAA

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ButtonBase from './ButtonBase';
 
-const Button = (props) => <ButtonBase {...props} />;
+const Button = props => <ButtonBase {...props} />;
 
 export default Button;
