3b569f158231bfa70b91b0c29221a35310c7f733
André Ruffert <github@andreruffert.com>
2021-09-24 18:41:21 +0200

    feat: introduce basic theme

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+export { default as theme } from './theme';
 // Components
 export { default as Button } from './Button';
---
+++ src/theme.js
@@ -0,0 +1,53 @@
+const colors = {
+  transparent: 'transparent',
+  black: '#0a0b0c',
+  white: '#ffffff',
+  gray: {
+    50: '#f5f5f5',
+    100: '#ececec',
+    300: '#9e9d9d',
+  },
+  purple: '#8d34ff',
+  green: '#00C64E',
+  yellow: '#ffd800',
+  blue: {
+    300: '#3ac4ff',
+  },
+  danger: '#fe016e',
+};
+
+const fontSizes = {
+  xs: 12,
+  sm: 14,
+  md: 16,
+  lg: 18,
+  xl: 20,
+  '2xl': 24,
+  '3xl': 30,
+  '4xl': 36,
+  '5xl': 48,
+  '6xl': 60,
+};
+
+const radii = {
+  none: 0,
+  xs: 4,
+  sm: 8,
+  md: 12,
+  lg: 16,
+  xl: 24,
+  '2xl': 32,
+  '3xl': 45,
+  full: 9999,
+};
+
+const space = [0, 4, 8, 16, 24, 32, 40, 48, 64, 80, 96, 112, 128];
+
+const theme = {
+  colors,
+  fontSizes,
+  radii,
+  space,
+};
+
+export default theme;
