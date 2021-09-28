2a322208d36f7c7698d6ec8f07480c9e841e7f1f
André Ruffert <github@andreruffert.com>
2021-09-28 23:32:37 +0200

    chore: introduce alias import paths
    // before
    import { Button } from '../src/'
    import componentMapping from '../playground/config'
    
    // after
    import { Button } from '@cash'
    import componentMapping from '@playground/config';

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@cash': './src',
+          '@playground': './playground',
+        },
+      },
+    ],
+  ],
 };
