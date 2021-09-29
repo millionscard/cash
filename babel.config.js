151807873560ffd67b1c1115170471319cacef27
André Ruffert <github@andreruffert.com>
2021-09-29 18:16:07 +0200

    perf: remove console statements in production

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,23 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@cash': './src',
-          '@playground': './playground',
-        },
+const presets = ['module:metro-react-native-babel-preset'];
+const plugins = [
+  [
+    'module-resolver',
+    {
+      alias: {
+        '@cash': './src',
+        '@playground': './playground',
       },
-    ],
+    },
   ],
+];
+
+module.exports = {
+  presets,
+  plugins,
+  env: {
+    production: {
+      presets,
+      plugins: [...plugins, ['transform-remove-console']],
+    },
+  },
 };
