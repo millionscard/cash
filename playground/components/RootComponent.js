2ac33f429b16bb8b0a21c5212690b7f93478af0e
André Ruffert <github@andreruffert.com>
2021-09-29 11:56:00 +0200

    feat(playground): persist navigation state

---
+++ playground/components/RootComponent.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { Linking } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NavigationContainer } from '@react-navigation/native';
+import RootNavigator from '@playground/components/RootNavigator';
+import { Box } from '@cash';
+
+const PERSISTENCE_KEY = 'NAVIGATION_STATE';
+
+const RootComponent = () => {
+  const [isReady, setIsReady] = useState(false);
+  const [initialState, setInitialState] = useState();
+
+  useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+        if (!initialUrl) {
+          // Only restore state if there's no deep link
+          const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+          const state = savedStateString ? JSON.parse(savedStateString) : undefined;
+          if (state !== undefined) {
+            setInitialState(state);
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return (
+      <Box flex={1} alignItems="center" justifyContent="center">
+        Loading...
+      </Box>
+    );
+  }
+
+  return (
+    <NavigationContainer
+      initialState={initialState}
+      onStateChange={state => AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))}
+    >
+      <RootNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default RootComponent;
