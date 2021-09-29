3842ce64556ed1630536824487c5c0d78eed9919
André Ruffert <github@andreruffert.com>
2021-09-29 21:49:27 +0200

    refactor: playground components config

--- playground/config.js
+++ playground/config.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Button, ButtonPrimary, Heading, Box, Card } from '@cash';
+import * as Cash from '@cash';
 
 const components = [
   {
@@ -10,18 +10,18 @@ const components = [
       {
         title: 'Button',
         description: '',
-        component: <Button background="white">Button</Button>,
+        component: <Cash.Button background="white">Button</Cash.Button>,
       },
       {
         title: 'Button Primary',
         description: '',
         component: (
           <>
-            <ButtonPrimary mb="3">Button Primary</ButtonPrimary>
-            <ButtonPrimary mb="3" colorScheme="gold">
+            <Cash.ButtonPrimary mb="3">Button Primary</Cash.ButtonPrimary>
+            <Cash.ButtonPrimary mb="3" colorScheme="gold">
               Button Primary
-            </ButtonPrimary>
-            <ButtonPrimary colorScheme="green">Button Primary</ButtonPrimary>
+            </Cash.ButtonPrimary>
+            <Cash.ButtonPrimary colorScheme="green">Button Primary</Cash.ButtonPrimary>
           </>
         ),
       },
@@ -36,9 +36,9 @@ const components = [
         title: 'Example',
         description: '',
         component: (
-          <Box bg="gray.100" p={3} _text={{ color: 'gray.300' }}>
+          <Cash.Box bg="gray.100" p={3} _text={{ color: 'gray.300' }}>
             Box
-          </Box>
+          </Cash.Box>
         ),
       },
     ],
@@ -49,30 +49,30 @@ const components = [
     docs: null,
     stories: [
       {
-        title: 'Text',
+        title: 'Example',
         description: '',
         component: (
           <>
-            <Box alignItems="center" mb={5}>
-              <Text fontWeight="bold">bold</Text>
-              <Text color="purple">purple color</Text>
-              <Text color="danger">danger color</Text>
-            </Box>
+            <Cash.Box alignItems="center" mb={5}>
+              <Cash.Text fontWeight="bold">bold</Cash.Text>
+              <Cash.Text color="purple">purple color</Cash.Text>
+              <Cash.Text color="danger">danger color</Cash.Text>
+            </Cash.Box>
 
-            <Box alignItems="center">
-              <Heading mb={2}>Sizes</Heading>
+            <Cash.Box alignItems="center">
+              <Cash.Heading mb={2}>Sizes</Cash.Heading>
 
-              <Text fontSize="xs">xs</Text>
-              <Text fontSize="sm">sm</Text>
-              <Text fontSize="md">md</Text>
-              <Text fontSize="lg">lg</Text>
-              <Text fontSize="xl">xl</Text>
-              <Text fontSize="2xl">2xl</Text>
-              <Text fontSize="3xl">3xl</Text>
-              <Text fontSize="4xl">4xl</Text>
-              <Text fontSize="5xl">5xl</Text>
-              <Text fontSize="6xl">6xl</Text>
-            </Box>
+              <Cash.Text fontSize="xs">xs</Cash.Text>
+              <Cash.Text fontSize="sm">sm</Cash.Text>
+              <Cash.Text fontSize="md">md</Cash.Text>
+              <Cash.Text fontSize="lg">lg</Cash.Text>
+              <Cash.Text fontSize="xl">xl</Cash.Text>
+              <Cash.Text fontSize="2xl">2xl</Cash.Text>
+              <Cash.Text fontSize="3xl">3xl</Cash.Text>
+              <Cash.Text fontSize="4xl">4xl</Cash.Text>
+              <Cash.Text fontSize="5xl">5xl</Cash.Text>
+              <Cash.Text fontSize="6xl">6xl</Cash.Text>
+            </Cash.Box>
           </>
         ),
       },
@@ -84,9 +84,9 @@ const components = [
     docs: null,
     stories: [
       {
-        title: 'Heading',
+        title: 'Example',
         description: '',
-        component: <Heading>Some default heading</Heading>,
+        component: <Cash.Heading>Some default heading</Cash.Heading>,
       },
     ],
   },
@@ -99,10 +99,10 @@ const components = [
         title: 'Example',
         description: '',
         component: (
-          <Card height={300} bg="yellow" justifyContent="space-between">
-            <Heading>Heading</Heading>
-            <Button>Some button</Button>
-          </Card>
+          <Cash.Card height={300} bg="yellow" justifyContent="space-between">
+            <Cash.Heading>Heading</Cash.Heading>
+            <Cash.Button>Some button</Cash.Button>
+          </Cash.Card>
         ),
       },
     ],
