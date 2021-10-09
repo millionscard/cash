ec9f1ba128093aa2a1ac0220cf40d43fd3ee87f4
André Ruffert <github@andreruffert.com>
2021-10-09 21:58:42 +0200

    fix(playground): "DocsLink" url

--- playground/components/DocsLink.js
+++ playground/components/DocsLink.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Pressable, Text, Linking } from 'react-native';
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://millionscard.github.io/cash';
 
 const DocsLink = ({ path }) => {
   if (!path) return null;
-  const url = `${BASE_URL}/${path}`;
+  const url = `${BASE_URL}${path}`;
   return (
     <Pressable onPress={() => Linking.openURL(url)}>
       <Text>Docs</Text>
