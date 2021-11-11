e029752cacb34c0c61ac3326b347e3a9be696f84
André Ruffert <github@andreruffert.com>
2021-11-11 11:08:08 +0100

    feat(theme): update radius, typography units

--- src/theme/radius.js
+++ src/theme/radius.js
@@ -1,13 +1,13 @@
 const radius = {
   none: 0,
-  xs: 4,
-  sm: 8,
-  md: 12,
-  lg: 16,
-  xl: 24,
-  '2xl': 30,
-  '3xl': 45,
-  full: 9999,
+  xs: '4px',
+  sm: '8px',
+  md: '12px',
+  lg: '16px',
+  xl: '24px',
+  '2xl': '30px',
+  '3xl': '45px',
+  full: '9999px',
 };
 
 export default radius;
