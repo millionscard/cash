24750e368493908dc488bea3696811d15bb2b993
André Ruffert <github@andreruffert.com>
2021-12-13 21:13:08 +0100

    feat(Heading): use platform specific theme fonts

--- src/Heading.js
+++ src/Heading.js
@@ -3,32 +3,29 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
 import { color, space, typography, variant } from 'styled-system';
 
-const StyledHeading = styled.Text(
-  {
-    fontFamily: 'Futura PT',
-    fontWeight: 'bold',
+const sizeVariants = variant({
+  prop: 'size',
+  variants: {
+    xs: { fontSize: 'md', lineHeight: '20px', letterSpacing: '-0.25px' },
+    sm: { fontSize: 'xl', lineHeight: '24px', letterSpacing: '-0.25px' },
+    md: { fontSize: '2xl', lineHeight: '24px', letterSpacing: '-0.5px' },
+    lg: { fontSize: '3xl', lineHeight: '28px', letterSpacing: '-0.5px' },
+    xl: { fontSize: '4xl', lineHeight: '32px', letterSpacing: '-0.5px' },
+    '2xl': { fontSize: '5xl', lineHeight: '36px', letterSpacing: '-1px' },
+    '3xl': { fontSize: '6xl', lineHeight: '48px', letterSpacing: '-2px' },
+    '4xl': { fontSize: '7xl', lineHeight: '56px', letterSpacing: '-3px' },
+    '5xl': { fontSize: '8xl', lineHeight: '72px', letterSpacing: '-4px' },
+    '6xl': { fontSize: '9xl', lineHeight: '88px', letterSpacing: '-4px' },
   },
-  color,
-  space,
-  typography,
-  variant({
-    prop: 'size',
-    variants: {
-      xs: { fontSize: 'md', lineHeight: '20px', letterSpacing: '-0.25px' },
-      sm: { fontSize: 'xl', lineHeight: '24px', letterSpacing: '-0.25px' },
-      md: { fontSize: '2xl', lineHeight: '24px', letterSpacing: '-0.5px' },
-      lg: { fontSize: '3xl', lineHeight: '28px', letterSpacing: '-0.5px' },
-      xl: { fontSize: '4xl', lineHeight: '32px', letterSpacing: '-0.5px' },
-      '2xl': { fontSize: '5xl', lineHeight: '36px', letterSpacing: '-1px' },
-      '3xl': { fontSize: '6xl', lineHeight: '48px', letterSpacing: '-2px' },
-      '4xl': { fontSize: '7xl', lineHeight: '56px', letterSpacing: '-3px' },
-      '5xl': { fontSize: '8xl', lineHeight: '72px', letterSpacing: '-4px' },
-      '6xl': { fontSize: '9xl', lineHeight: '88px', letterSpacing: '-4px' },
-    },
-  })
-);
+});
 
-const Heading = props => <StyledHeading {...props} />;
+const Heading = styled.Text`
+  ${({ theme }) => theme.fonts.FuturaPT.bold};
+  ${color}
+  ${space}
+  ${typography}
+  ${sizeVariants}
+`;
 
 Heading.propTypes = {
   /** results in smaller or larger Heading text size */
