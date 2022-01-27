80806400e3b36a69aa836eef91db36697c8a29fe
André Ruffert <github@andreruffert.com>
2022-01-27 01:15:00 +0100

    x

--- playground/components/DocsLink.js
+++ playground/components/DocsLink.js
@@ -4,7 +4,9 @@ import { Pressable, Text, Linking } from 'react-native';
 const BASE_URL = 'https://millionscard.github.io/cash';
 
 const DocsLink = ({ path }) => {
-  if (!path) return null;
+  if (!path) {
+    return null;
+  }
   const url = `${BASE_URL}${path}`;
   return (
     <Pressable onPress={() => Linking.openURL(url)}>
