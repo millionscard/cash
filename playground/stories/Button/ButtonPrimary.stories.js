876f72985057c2b3ae4fbcb8268c2c40f03b61ee
André Ruffert <github@andreruffert.com>
2022-04-12 14:23:58 +0200

    feat(playground): add "ButtonPrimary" sizes

--- playground/stories/Button/ButtonPrimary.stories.js
+++ playground/stories/Button/ButtonPrimary.stories.js
@@ -17,22 +17,31 @@ export default () => {
 
   return (
     <>
-      <Cash.ButtonPrimary mb="3" isLoading={isLoading} onPress={triggerLoadingState} isFullWidth>
+      <Cash.ButtonPrimary mb="3" isLoading={isLoading} onPress={triggerLoadingState}>
         Button text
       </Cash.ButtonPrimary>
-      <Cash.ButtonPrimary colorScheme="gold" mb="3" isFullWidth hasShadow>
+      <Cash.ButtonPrimary colorScheme="gold" mb="3" hasShadow>
         Button text
       </Cash.ButtonPrimary>
-      <Cash.ButtonPrimary colorScheme="danger" mb="3" isFullWidth>
+      <Cash.ButtonPrimary colorScheme="danger" mb="3">
         Button text
       </Cash.ButtonPrimary>
-      <Cash.ButtonPrimary colorScheme="success" mb="3" isFullWidth>
+      <Cash.ButtonPrimary colorScheme="success" mb="3">
         Button text
       </Cash.ButtonPrimary>
-      <Cash.ButtonPrimary mb="3">Button text</Cash.ButtonPrimary>
-      <Cash.ButtonPrimary mb="3" size="md">
-        Button text
-      </Cash.ButtonPrimary>
+
+      <Cash.Card bg="black.25">
+        <Cash.Heading mb="10px">Sizes</Cash.Heading>
+        <Cash.ButtonPrimary mb="3" size="lg" isFullWidth={false}>
+          Button text
+        </Cash.ButtonPrimary>
+        <Cash.ButtonPrimary mb="3" size="md" isFullWidth={false}>
+          Button text
+        </Cash.ButtonPrimary>
+        <Cash.ButtonPrimary mb="3" size="sm" isFullWidth={false}>
+          Button text
+        </Cash.ButtonPrimary>
+      </Cash.Card>
     </>
   );
 };
