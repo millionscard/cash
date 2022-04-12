89d48ef7c2413d06ef1119f0e2b07460ba51a8df
André Ruffert <github@andreruffert.com>
2022-04-12 22:17:57 +0200

    feat(Heading)!: update supported props

--- playground/stories/Heading.stories.js
+++ playground/stories/Heading.stories.js
@@ -1,37 +1,27 @@
 import React from 'react';
 import * as Cash from '@cash';
 
+const sizes = ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl'];
+const weights = ['light', 'book', 'medium', 'demi', 'bold', 'heavy', 'extrabold'];
+
 export default () => (
   <>
-    <Cash.Heading numberOfLines={1} size="xs">
-      A million ways to win
-    </Cash.Heading>
-    <Cash.Heading numberOfLines={1} size="sm">
-      A million ways to win
-    </Cash.Heading>
-    <Cash.Heading numberOfLines={1} size="md">
-      A million ways to win
-    </Cash.Heading>
-    <Cash.Heading numberOfLines={1} size="lg">
-      A million ways to win
-    </Cash.Heading>
-    <Cash.Heading numberOfLines={1} size="xl">
-      A million ways to win
-    </Cash.Heading>
-    <Cash.Heading numberOfLines={1} size="2xl">
-      A million ways to win
-    </Cash.Heading>
-    <Cash.Heading numberOfLines={1} size="3xl">
-      A million ways to win
-    </Cash.Heading>
-    <Cash.Heading numberOfLines={1} size="4xl">
-      A million ways to win
-    </Cash.Heading>
-    <Cash.Heading numberOfLines={1} size="5xl">
-      A million ways to win
-    </Cash.Heading>
-    <Cash.Heading numberOfLines={1} size="6xl">
-      A million ways to win
-    </Cash.Heading>
+    <Cash.Card mb={3} bg="black.10">
+      <Cash.Heading mb={2}>Sizes</Cash.Heading>
+      {sizes.map(size => (
+        <Cash.Heading numberOfLines={1} size={size} key={size}>
+          A million ways to win
+        </Cash.Heading>
+      ))}
+    </Cash.Card>
+
+    <Cash.Card bg="black.10">
+      <Cash.Heading mb={2}>Weights</Cash.Heading>
+      {weights.map(weight => (
+        <Cash.Heading weight={weight} key={weight}>
+          {weight}
+        </Cash.Heading>
+      ))}
+    </Cash.Card>
   </>
 );
