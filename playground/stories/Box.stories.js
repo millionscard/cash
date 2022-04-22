8cb103fa3900f0460a00229138e4a9354e5fbf8c
André Ruffert <github@andreruffert.com>
2022-04-22 22:07:53 +0200

    feat(Box): add bg gradient support

--- playground/stories/Box.stories.js
+++ playground/stories/Box.stories.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import * as Cash from '@cash';
 
-export default () => (
-  <Cash.Box bg="black.10" p={3} _text={{ color: 'black.300' }} sx={{ bg: 'purple.100' }}>
-    Box
-  </Cash.Box>
-);
+export default () => {
+  const { gradients } = Cash.useTheme();
+
+  return (
+    <Cash.Stack direction="column" gap="20px">
+      <Cash.Box bg="black.10" p={3} _text={{ color: 'black.300' }} sx={{ bg: 'purple.100' }}>
+        Box
+      </Cash.Box>
+      <Cash.Box bgGradient={gradients.purple} p={3} _text={{ color: 'white' }}>
+        Box with bgGradient
+      </Cash.Box>
+      <Cash.Box bgGradient={gradients.pink} p={3} _text={{ color: 'white' }} borderRadius="md">
+        Box rounded with bgGradient
+      </Cash.Box>
+    </Cash.Stack>
+  );
+};
