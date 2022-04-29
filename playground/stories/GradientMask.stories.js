58d1fda7b64e0d3957c277f643666a55c33628dc
André Ruffert <github@andreruffert.com>
2022-04-29 22:27:24 +0200

    chore(playground): story updates

--- playground/stories/GradientMask.stories.js
+++ playground/stories/GradientMask.stories.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import * as Cash from '@cash';
 
-export default () => (
-  <Cash.Stack direction="column" gap="16px">
-    <Cash.GradientMask>Text</Cash.GradientMask>
-    <Cash.GradientMask>
-      <Cash.Heading>Heading</Cash.Heading>
-    </Cash.GradientMask>
-    <Cash.GradientMask>
-      <Cash.Icon name="Backspace" width="32" />
-    </Cash.GradientMask>
-  </Cash.Stack>
-);
+export default () => {
+  const { gradients } = Cash.useTheme();
+
+  return (
+    <Cash.Stack direction="column" gap="16px">
+      <Cash.GradientMask>Text ✨</Cash.GradientMask>
+      <Cash.GradientMask {...gradients.green}>
+        <Cash.Heading size="xl" weight="bold">
+          $100
+        </Cash.Heading>
+      </Cash.GradientMask>
+      <Cash.GradientMask {...gradients.fire}>
+        <Cash.Icon name="Backspace" width="32" />
+      </Cash.GradientMask>
+    </Cash.Stack>
+  );
+};
--- playground/stories/Stack.stories.js
+++ playground/stories/Stack.stories.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import * as Cash from '@cash';
-import BoxExample from './Box.stories';
 
+const BoxExample = props => (
+  <Cash.Box bg="black.50" width="50px" height="50px" {...props}></Cash.Box>
+);
 export default () => (
   <>
-    <Cash.Stack direction="row" gap="20px" mb={3}>
-      {Array.from({ length: 3 }, (_, id) => ({ id })).map(({ id }) => (
-        <BoxExample key={id} />
-      ))}
-    </Cash.Stack>
-    <Cash.Stack direction="column" gap="20px">
-      {Array.from({ length: 3 }, (_, id) => ({ id })).map(({ id }) => (
-        <BoxExample key={id} />
-      ))}
+    <Cash.Stack direction="column" gap="20px" mb={3}>
+      <Cash.Heading>Direction: row</Cash.Heading>
+      <Cash.Stack direction="row" gap="20px" mb={3}>
+        {Array.from({ length: 3 }, (_, id) => ({ id })).map(({ id }) => (
+          <BoxExample key={id} width="25%" />
+        ))}
+      </Cash.Stack>
+
+      <Cash.Heading>Direction: column</Cash.Heading>
+      <Cash.Stack direction="column" gap="20px">
+        {Array.from({ length: 3 }, (_, id) => ({ id })).map(({ id }) => (
+          <BoxExample key={id} width="100%" />
+        ))}
+      </Cash.Stack>
     </Cash.Stack>
   </>
 );
