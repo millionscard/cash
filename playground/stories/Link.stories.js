1d8283ab827726b2b89af638c1f02522db60404d
André Ruffert <github@andreruffert.com>
2022-05-05 22:26:40 +0200

    feat(Link): accept Text props

--- playground/stories/Link.stories.js
+++ playground/stories/Link.stories.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import * as Cash from '@cash';
 
-export default () => <Cash.Link url="millions://home">Link</Cash.Link>;
+export default () => (
+  <Cash.Stack direction="column" gap="20px">
+    <Cash.Link url="millions://home">Link</Cash.Link>
+    <Cash.Link url="millions://home" color="teal.600" shadow="md">
+      Link
+    </Cash.Link>
+    <Cash.Link url="millions://home" size="xs">
+      Link
+    </Cash.Link>
+  </Cash.Stack>
+);
