a497e392829bb4d74e1465d36fd8c4bf0cd2444c
André Ruffert <github@andreruffert.com>
2022-05-13 14:07:08 +0200

    refactor(TextDivider): require cycle

--- src/TextDivider.js
+++ src/TextDivider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
-import { Box } from '@millionscard/cash';
+import Box from './Box';
 
 const Divider = styled(Box)({
   flexDirection: 'row',
