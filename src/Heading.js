66cf3da56910d1cca2dc49ca11a808ed4d24648c
André Ruffert <github@andreruffert.com>
2022-05-19 13:22:38 +0200

    perf(Heading): compose system props

--- src/Heading.js
+++ src/Heading.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
-import { variant, color, space, system, lineHeight } from 'styled-system';
+import { variant, color, space, lineHeight, system, compose } from 'styled-system';
 
-const sizeVariants = variant({
-  prop: 'size',
-  variants: {
-    xs: { fontSize: 'md', lineHeight: '20px', letterSpacing: '-0.25px' },
-    sm: { fontSize: 'xl', lineHeight: '24px', letterSpacing: '-0.25px' },
-    md: { fontSize: '2xl', lineHeight: '24px', letterSpacing: '-0.5px' },
-    lg: { fontSize: '3xl', lineHeight: '28px', letterSpacing: '-0.5px' },
-    xl: { fontSize: '4xl', lineHeight: '32px', letterSpacing: '-0.5px' },
-    '2xl': { fontSize: '5xl', lineHeight: '38px', letterSpacing: '-1px' },
-    '3xl': { fontSize: '6xl', lineHeight: '50px', letterSpacing: '-2px' },
-    '4xl': { fontSize: '7xl', lineHeight: '62px', letterSpacing: '-3px' },
-    '5xl': { fontSize: '8xl', lineHeight: '82px', letterSpacing: '-4px' },
-    '6xl': { fontSize: '9xl', lineHeight: '98px', letterSpacing: '-4px' },
-  },
-});
-
-const StyledHeading = styled.Text.attrs({ allowFontScaling: false })`
-  font-family: ${({ theme, weight }) =>
-    theme.fonts.FuturaPT?.[weight] || theme.fonts.FuturaPT.demi};
-  ${system({
-    align: {
-      property: 'textAlign',
-    },
-    shadow: {
-      property: 'textShadow',
-      scale: 'shadows',
-    },
-  })}
-  ${sizeVariants}
-  ${color}
-  ${lineHeight}
-  ${space}
-`;
+const StyledHeading = styled.Text.attrs({ allowFontScaling: false })(
+  ({ theme, weight }) => ({
+    fontFamily: theme.fonts.FuturaPT?.[weight] || theme.fonts.FuturaPT.demi,
+  }),
+  compose(
+    color,
+    space,
+    lineHeight,
+    system({
+      align: {
+        property: 'textAlign',
+      },
+      shadow: {
+        property: 'textShadow',
+        scale: 'shadows',
+      },
+    }),
+    variant({
+      prop: 'size',
+      variants: {
+        xs: { fontSize: 'md', lineHeight: '20px', letterSpacing: '-0.25px' },
+        sm: { fontSize: 'xl', lineHeight: '24px', letterSpacing: '-0.25px' },
+        md: { fontSize: '2xl', lineHeight: '24px', letterSpacing: '-0.5px' },
+        lg: { fontSize: '3xl', lineHeight: '28px', letterSpacing: '-0.5px' },
+        xl: { fontSize: '4xl', lineHeight: '32px', letterSpacing: '-0.5px' },
+        '2xl': { fontSize: '5xl', lineHeight: '38px', letterSpacing: '-1px' },
+        '3xl': { fontSize: '6xl', lineHeight: '50px', letterSpacing: '-2px' },
+        '4xl': { fontSize: '7xl', lineHeight: '62px', letterSpacing: '-3px' },
+        '5xl': { fontSize: '8xl', lineHeight: '82px', letterSpacing: '-4px' },
+        '6xl': { fontSize: '9xl', lineHeight: '98px', letterSpacing: '-4px' },
+      },
+    })
+  )
+);
 
 const Heading = ({ children, ...props }) => <StyledHeading {...props}>{children}</StyledHeading>;
 
